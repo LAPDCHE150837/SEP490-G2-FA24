0c32d16d43bf48b4da9b0d88c3a20de344851a2e
LAPDCHE150837 <lapdche150837@fpt.edu.vn>
2024-10-17 23:58:58 +0700

    init master

---
+++ src/main/java/com/nihongo/sep490g2fa24/Sep490G2Fa24Application.java
@@ -0,0 +1,13 @@
+package com.nihongo.sep490g2fa24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sep490G2Fa24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sep490G2Fa24Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/nihongo/sep490g2fa24/Sep490G2Fa24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nihongo.sep490g2fa24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sep490G2Fa24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
