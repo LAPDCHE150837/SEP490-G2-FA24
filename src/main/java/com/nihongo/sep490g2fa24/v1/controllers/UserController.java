9cd5906bee05ab5f8861756cc68f61469707ac81
xxduog2702 <duongtxhe160708@fpt.edu.vn>
2024-10-21 15:49:29 +0700

    fix feature login #3

--- src/main/java/com/nihongo/sep490g2fa24/v1/controllers/UserController.java
+++ src/main/java/com/nihongo/sep490g2fa24/v1/controllers/UserController.java
@@ -1,6 +1,5 @@
 package com.nihongo.sep490g2fa24.v1.controllers;
 
-import com.nihongo.sep490g2fa24.v1.dtos.response.user.LoginResponse;
 import com.nihongo.sep490g2fa24.v1.services.AuthenService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
