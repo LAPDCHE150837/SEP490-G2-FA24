9df9967ca56c8414a2897c6fe2a5d4d25e73e2cc
PhuocNHHE163692 <147647641+PhuocNHHE163692@users.noreply.github.com>
2024-10-22 22:35:28 +0700

    dev Api change password #2

---
+++ src/main/java/com/nihongo/sep490g2fa24/v1/dtos/request/ChangePasswordRequest.java
@@ -0,0 +1,16 @@
+package com.nihongo.sep490g2fa24.v1.dtos.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChangePasswordRequest {
+    private String oldPassword;
+    private String newPassword;
+    private String email;
+}
