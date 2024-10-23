3e9937edca6dafc2b6af8fbea071d6824b140f9e
THINHNDHE160416 <thinhndhe160416@fpt.edu.vn>
2024-10-23 18:43:56 +0700

    fix authen process #2

--- src/main/java/com/nihongo/sep490g2fa24/exception/NhgErrorHandler.java
+++ src/main/java/com/nihongo/sep490g2fa24/exception/NhgErrorHandler.java
@@ -8,14 +8,15 @@ public enum NhgErrorHandler implements INhgErrorHandler{
     INVALID_INPUT("MEA000001", "common.apiResponse.invalidInput" ),
     UNAUTHORIZED("Unauthorized", "common.apiResponse.unauthorized"),
     LOGIN_ERROR("MEI009023", "Login error" ),
-    EMAIL_NOT_FOUND("MEA000002", "common.apiResponse.emailNotFound" ),
+    USERNAME_NOT_FOUND("MEA000002", "common.apiResponse.usernameNotFound" ),
     INVALID_CREDENTIALS("MEA000003", "common.apiResponse.invalidCredentials" ),
     TOKEN_INVALID("MEA000003", "common.apiResponse.tokenInvalid" ),
     USER_IS_EXISTED("MEA000004", "common.apiResponse.userIsExisted" ),
     SEND_MAIL_ERROR("MEA000005", "common.apiResponse.sendMailError"),
     USER_IS_VERIFIED("MEA000006", "common.apiResponse.userIsVerified" ),
     INVALID_PASSWORD("MEA000007", "common.apiResponse.invalidPassword" ),
-    ;
+    INVALID_ROLE("MEA000008","common.apiResponse.invalidRole" ),
+    EMAIL_NOT_FOUND("MEA000009", "common.apiResponse.emailNotFound");
 
     private final String code;
     private final String message;
