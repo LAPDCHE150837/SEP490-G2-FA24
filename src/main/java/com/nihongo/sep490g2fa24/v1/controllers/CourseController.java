3087fae1781f0879693b877b92f03b9e76395484
xxduog2702 <duongtxhe160708@fpt.edu.vn>
2024-11-05 10:26:54 +0700

    Course backend

--- src/main/java/com/nihongo/sep490g2fa24/v1/controllers/CourseController.java
+++ src/main/java/com/nihongo/sep490g2fa24/v1/controllers/CourseController.java
@@ -3,44 +3,42 @@ package com.nihongo.sep490g2fa24.v1.controllers;
 import com.nihongo.sep490g2fa24.v1.dtos.course.CourseDTO;
 import com.nihongo.sep490g2fa24.v1.repositories.CourseRepository;
 import com.nihongo.sep490g2fa24.v1.services.CourseService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/course")
+@RequiredArgsConstructor
 public class CourseController {
 
-    private final CourseService courseService ;
-
-
-    public CourseController(CourseService courseService) {
-        this.courseService = courseService;
-    }
-
+    private final CourseService courseService;
 
     @GetMapping
-    public List<CourseDTO> getAllCourse() {
-        return courseService.getAllCourse();
+    public BaseApiResponse<List<CourseDTO>> getAllCourse() {
+        return BaseApiResponse.succeed(courseService.getAllCourse());
     }
 
 
     @PostMapping
-    public void addCourse(@RequestBody CourseDTO courseDTO) {
+    public BaseApiResponse<Void> addCourse(@RequestBody CourseDTO courseDTO) {
         courseService.addCourse(courseDTO);
-
+        return BaseApiResponse.succeed();
     }
 
     @DeleteMapping("/{courseId}")
-    public void deleteCourse(@PathVariable("courseId") String courseId) {
+    public BaseApiResponse<Void> deleteCourse(@PathVariable("courseId") String courseId) {
         courseService.deleteCourseById(courseId);
+        return BaseApiResponse.succeed();
     }
 
     @PutMapping("/{courseId}")
-    public void updateCourse(
+    public BaseApiResponse<Void> updateCourse(
             @PathVariable("courseId") String courseId,
             @RequestBody CourseDTO courseDTO) {
         courseService.updateCourseById(courseId, courseDTO);
+        return BaseApiResponse.succeed();
     }
 
 
