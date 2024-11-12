16aa85a9ff28617d76bec663f4b4d2631b4df1ef
PhuocNHHE163692 <147647641+PhuocNHHE163692@users.noreply.github.com>
2024-11-13 00:09:39 +0700

    update api course #2

--- src/main/java/com/nihongo/sep490g2fa24/v1/dtos/course/CourseDTO.java
+++ src/main/java/com/nihongo/sep490g2fa24/v1/dtos/course/CourseDTO.java
@@ -11,6 +11,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CourseDTO {
+    @NotNull
     private String id;
     private String courseName;
     private String flagActive;
