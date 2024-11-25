c2bf13f6018fc68d93433449a242ba84ac9f868f
PhuocNHHE163692 <147647641+PhuocNHHE163692@users.noreply.github.com>
2024-11-26 00:43:37 +0700

    Update Grammar BE

--- src/main/java/com/nihongo/sep490g2fa24/v1/controllers/GrammarController.java
+++ src/main/java/com/nihongo/sep490g2fa24/v1/controllers/GrammarController.java
@@ -4,7 +4,6 @@ import com.nihongo.sep490g2fa24.v1.dtos.course.GrammarDTO;
 import com.nihongo.sep490g2fa24.v1.model.Grammar;
 import com.nihongo.sep490g2fa24.v1.services.impl.GrammarService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,28 +15,28 @@ public class GrammarController {
     private final GrammarService grammarService;
 
     @GetMapping
-    public ResponseEntity<List<GrammarDTO>> getAllGrammars() {
-        return ResponseEntity.ok(grammarService.getAllGrammars());
+    public BaseApiResponse<List<GrammarDTO>> getAllGrammars() {
+        return BaseApiResponse.succeed(grammarService.getAllGrammars());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<GrammarDTO> getGrammarById(@PathVariable String id) {
-        return ResponseEntity.ok(grammarService.getGrammarById(id));
+    public BaseApiResponse<GrammarDTO> getGrammarById(@PathVariable String id) {
+        return BaseApiResponse.succeed(grammarService.getGrammarById(id));
     }
 
     @PostMapping
-    public ResponseEntity<Grammar> createGrammar(@RequestBody Grammar grammar) {
-        return ResponseEntity.ok(grammarService.createGrammar(grammar));
+    public BaseApiResponse<Grammar> createGrammar(@RequestBody Grammar grammar) {
+        return BaseApiResponse.succeed(grammarService.createGrammar(grammar));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Grammar> updateGrammar(@PathVariable String id, @RequestBody Grammar grammar) {
-        return ResponseEntity.ok(grammarService.updateGrammar(id, grammar));
+    public BaseApiResponse<Grammar> updateGrammar(@PathVariable String id, @RequestBody Grammar grammar) {
+        return BaseApiResponse.succeed(grammarService.updateGrammar(id, grammar));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteGrammar(@PathVariable String id) {
+    public BaseApiResponse<Void> deleteGrammar(@PathVariable String id) {
         grammarService.deleteGrammar(id);
-        return ResponseEntity.ok().build();
+        return BaseApiResponse.succeed() ;
     }
 }
