bb39f5968de34044dc015d7ddec2ec26acac7947
THINHNDHE160416 <thinhndhe160416@fpt.edu.vn>
2024-12-04 00:38:10 +0700

    Modify User Progress #3

--- src/main/java/com/nihongo/sep490g2fa24/v1/controllers/UserProgressController.java
+++ src/main/java/com/nihongo/sep490g2fa24/v1/controllers/UserProgressController.java
@@ -1,7 +1,11 @@
 package com.nihongo.sep490g2fa24.v1.controllers;
 
+import com.nihongo.sep490g2fa24.dtoMapper.LessonMapper;
+import com.nihongo.sep490g2fa24.v1.dtos.course.LessonDetailDTO;
 import com.nihongo.sep490g2fa24.v1.dtos.course.UserProgressDTO;
+import com.nihongo.sep490g2fa24.v1.model.Lesson;
 import com.nihongo.sep490g2fa24.v1.model.UserProgress;
+import com.nihongo.sep490g2fa24.v1.repositories.UserProgressRepository;
 import com.nihongo.sep490g2fa24.v1.services.impl.UserProgressService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -10,16 +14,23 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/user-progress")
 @RequiredArgsConstructor
 public class UserProgressController {
     private final UserProgressService userProgressService;
+    private final UserProgressRepository userProgressRepository;
+    private final LessonMapper lessonMapper;
 
     @GetMapping
-    public ResponseEntity<List<UserProgressDTO>> getAllUserProgress() {
-        return ResponseEntity.ok(userProgressService.getAllUserProgress());
+    public BaseApiResponse<List<LessonDetailDTO>> getAllLessons() {
+        List<Lesson> lessons = userProgressRepository.findCompletedLessons();
+        List<LessonDetailDTO> lessonDTOs = lessons.stream()
+                .map(lessonMapper::toDetailDTO)
+                .collect(Collectors.toList());
+        return BaseApiResponse.succeed(lessonDTOs);
     }
 
     @GetMapping("/{id}")
@@ -33,20 +44,19 @@ public class UserProgressController {
     }
 
     @GetMapping("/lesson/{lessonId}")
-    public ResponseEntity<UserProgressDTO> getUserProgressByUserAndLessonId(@PathVariable String lessonId, HttpServletRequest request) {
+    public ResponseEntity<UserProgressDTO> getUserProgressByUserAndLessonId(@PathVariable String lessonId,HttpServletRequest request) {
         return ResponseEntity.ok(userProgressService.getUserProgressByUserAndLessonId(lessonId,request.getRemoteUser()));
     }
 
     @PostMapping
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
     public ResponseEntity<UserProgress> createUserProgress(
             @RequestBody UserProgress userProgress,
+            String lessonId,
             HttpServletRequest request
     ) {
         return ResponseEntity.ok(userProgressService.createUserProgress(userProgress, request.getRemoteUser()));
     }
-
-
 
     @PutMapping("/lesson/{id}")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
