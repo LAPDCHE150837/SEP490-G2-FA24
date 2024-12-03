88bd8222a7c7bbb8227e992a1ca63709b37cfcf0
LAPDCHE150837 <lapdche150837@fpt.edu.vn>
2024-12-03 22:47:45 +0700

    Update authentication #3

--- src/main/java/com/nihongo/sep490g2fa24/v1/dtos/users/Permission.java
+++ src/main/java/com/nihongo/sep490g2fa24/v1/dtos/users/Permission.java
@@ -2,7 +2,7 @@ package com.nihongo.sep490g2fa24.v1.dtos.users;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-@Getter
+
 @RequiredArgsConstructor
 public enum Permission {
     ADMIN_READ("admin:read"),
@@ -12,8 +12,11 @@ public enum Permission {
     MANAGER_READ("management:read"),
     MANAGER_UPDATE("management:update"),
     MANAGER_CREATE("management:create"),
-    MANAGER_DELETE("management:delete")
-    ;
+    MANAGER_DELETE("management:delete"),
+    CONTENT_READ("content:read"),
+    CONTENT_UPDATE("content:update"),
+    CONTENT_CREATE("content:create"),
+    CONTENT_DELETE("content:delete");
     @Getter
-    private final String permisstion;
+    private final String permission;
 }
