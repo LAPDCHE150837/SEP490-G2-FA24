574bfb710353607ab4623ef2911e516369f87629
LAPDCHE150837 <lapdche150837@fpt.edu.vn>
2024-12-03 22:48:08 +0700

    Update authentication #4

--- src/main/java/com/nihongo/sep490g2fa24/v1/dtos/users/Role.java
+++ src/main/java/com/nihongo/sep490g2fa24/v1/dtos/users/Role.java
@@ -9,39 +9,21 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import static com.nihongo.sep490g2fa24.v1.dtos.users.Permission.*;
-
-
 @RequiredArgsConstructor
 public enum Role {
     USER(Collections.emptySet()),
-    ADMIN(
-            Set.of
-                    (
-                            ADMIN_READ,
-                            ADMIN_UPDATE,
-                            ADMIN_DELETE,
-                            ADMIN_CREATE,
-                            MANAGER_READ,
-                            MANAGER_UPDATE,
-                            MANAGER_DELETE,
-                            MANAGER_CREATE
-                    )
-    ),
-    MANAGER(
-            Set.of(
-                    MANAGER_READ,
-                    MANAGER_UPDATE,
-                    MANAGER_DELETE,
-                    MANAGER_CREATE
-            )
-    );
+    ADMIN(Collections.emptySet()),
+    MANAGER(Collections.emptySet()),
+    CONTENT(Collections.emptySet()),
+    STUDENT(Collections.emptySet()),
+    TEACHER(Collections.emptySet());
+
     @Getter
     private final Set<Permission> permissions;
 
     public List<SimpleGrantedAuthority> getAuthorities() {
         List<SimpleGrantedAuthority> authorities = getPermissions().stream()
-                .map(permission -> new SimpleGrantedAuthority(permission.getPermisstion()))
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
                 .collect(Collectors.toList());
         authorities.add(new SimpleGrantedAuthority("ROLE_" + name()));
         return authorities;
