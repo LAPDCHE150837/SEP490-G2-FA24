23b50e9f99e89371545df7249e62ef5da2abb0c6
LAPDCHE150837 <lapdche150837@fpt.edu.vn>
2024-12-03 22:45:27 +0700

    Update authentication #1

--- src/main/java/com/nihongo/sep490g2fa24/v1/services/AuthenService.java
+++ src/main/java/com/nihongo/sep490g2fa24/v1/services/AuthenService.java
@@ -1,5 +1,6 @@
 package com.nihongo.sep490g2fa24.v1.services;
 
+import com.nihongo.sep490g2fa24.v1.dtos.course.UserDTO;
 import com.nihongo.sep490g2fa24.v1.dtos.request.ChangePasswordRequest;
 import com.nihongo.sep490g2fa24.v1.dtos.request.LoginRequest;
 import com.nihongo.sep490g2fa24.v1.dtos.request.RegisterRequest;
@@ -9,12 +10,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 
+import java.util.List;
+
 public interface AuthenService{
-
-
     LoginResponse login(OAuth2AuthenticationToken authentication);
+
     LoginResponse register(RegisterRequest registerRequest, HttpServletRequest httpServletRequest);
+
     LoginResponse authenticate(LoginRequest loginRequest);
+
     String verifyEmail(String token);
     void saveUserToken(User user, String jwtToken);
     void changePassword(ChangePasswordRequest changePasswordRequest);
@@ -22,4 +26,6 @@ public interface AuthenService{
     LoginResponse forgotPassword(ChangePasswordRequest changePasswordRequest, HttpServletRequest httpServletRequest);
 
     void resetPassword(String token, HttpServletResponse response, ChangePasswordRequest changePasswordRequest);
+
+    List<UserDTO> getAll();
 }
